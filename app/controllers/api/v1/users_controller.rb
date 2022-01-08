1a85988a176894b9cd83ab3d7f0d8487b9bad4f4
Josy <josyanjos.souza@gmail.com>
2022-01-08 15:35:22 -0300

    Adds update and destroy actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class  Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update destroy]
+
   def create
     @user = User.new(user_params)
 
@@ -9,13 +11,30 @@ class  Api::V1::UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    head 204
+  end
+
   def show
-    render json: User.find(params[:id])
+    render json: @user
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -2,11 +2,11 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
   end
-    
+
   test "should show user" do
     get api_v1_user_url(@user), as: :json
     assert_response :success
-  
+
     json_response = JSON.parse(self.response.body)
     assert_equal @user.email, json_response['email']
   end
@@ -23,5 +23,22 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
       post api_v1_users_url, params: { user: { email: @user.email, password: '123456' } }, as: :json
     end
     assert_response :unprocessable_entity
+  end
+
+  test "should update user" do
+    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: '123456' } }, as: :json
+    assert_response :success
+  end
+
+  test "should not update user when invalid params are sent" do
+    patch api_v1_user_url(@user), params: { user: { email: 'bad_email', password: '123456' } }, as: :json
+    assert_response :unprocessable_entity
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete api_v1_user_url(@user), as: :json
+    end
+    assert_response :no_content
   end
 end
